ac2dcb749e896786e21a84686916067f728ae88b
timothyjgrant11@gmail.com <timothyjgrant11@gmail.com>
2019-03-30 17:37:03 -0400

    multiple search & other changes

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
 const apiKey = 'As6CAPSV8yCUHyJnzYOBfAnk8VAMtFnA0UkUEPD9'; 
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
-
-function getParks(query, maxResults) {
-    const params = {
-        api_key: apiKey,
-        stateCode: query,
-        part: 'snippit',
-        limit: maxResults
-    };
-    
-    const queryString = formatQueryParams(params);
-    const url = searchURL + "?" + queryString;
-    
-    fetch(url)
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        } throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-        .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-          });
-}
-
-function displayResults(responseJson){
-    console.log(responseJson);
-    $('#results').empty();
-    
-    for (let i=0; i < responseJson.data.length; i++) {
-    $('#results').append(
-    `<h2>${responseJson.data[i].fullName}</h2>
-    <p>${responseJson.data[i].description}</p>
-    <a href="${responseJson.data[i].url}">Link</a>`)};
-    
-    $('#results').removeClass('hidden');
-}
-
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
+}
+
+function displayParks(responseJson) {
+    console.log(responseJson);
+    $('#results-list').empty();
+
+    for (let i = 0; i < responseJson.data.length; i++) {
+        $('#results-list').append(
+            `<li><h3>${responseJson.data[i].name}</h3>
+            <p>${responseJson.data[i].url}</p>
+            <p>${responseJson.data[i].description}<p/>
+            </li>`
+          )};
+
+    $('#results').removeClass('hidden');   
+    }
+
+function getParks (query, limit=10) {
+    const params = {
+        api_key: apiKey,
+        limit,
+        stateCode:query 
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+
+    console.log(url);
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayParks(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
 }
 
 function watchForm() {
     $('form').submit(event => {
-        event.preventDefault();
-        let stateInput = $('#userInput').val();
-        let maxNumber = $('#numberInput').val();
-        console.log(stateInput);
-        console.log(maxNumber);
-        getParks(stateInput, maxNumber - 1);
+      event.preventDefault();
+      const searchState = $('#js-search-state').val();
+      const limit = $('#js-limit').val() - 1;
+      getParks(searchState, limit);
     });
-}
-
-$(watchForm);
+  }
+  
+  $(watchForm);
